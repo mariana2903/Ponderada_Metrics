4b0f1549680edc71e85a75d78c055e7c00721818
Mariana de Paula <99264876+mariana2903@users.noreply.github.com>
2024-05-13 01:11:52 -0300

    add código tutorial

---
+++ PonderadaMetricas-pt2/PonderadaMetricas-pt2/HatCoMetrics.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics.Metrics;
+
+public class HatCoMetrics
+{
+    private readonly Counter<int> _hatsSold;
+
+    public HatCoMetrics(IMeterFactory meterFactory)
+    {
+        var meter = meterFactory.Create("HatCo.Store");
+        _hatsSold = meter.CreateCounter<int>("hatco.store.hats_sold");
+    }
+
+    public void HatsSold(int quantity)
+    {
+        _hatsSold.Add(quantity);
+    }
+}
